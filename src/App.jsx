f8203ba7910b5a0e845376b436ab0e7d85c3fc7b
Hemanth-Kumar-04 <hemanthkumar.k.r04@gmail.com>
2024-04-04 20:27:28 +0530

    bento grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import React from "react";
+import Gallery from "./components/Gallery";
 
 function App() {
-
   return (
     <div className="App">
       Hello
+      <Gallery />
     </div>
   );
 }
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-
+import './Gallery.css'
 function Gallery(){
     return(
-      <div>
+      <div className="boduy">
+
+      <div className="container">
+        <div className="col-left">
+           <div className="row row1"></div>
+           <div className="row row2"></div>
+           <div className="row row3">
+               <div className="row-col left"></div>
+               <div className="row-col left"></div>
+           </div>
+        </div>
+        <div className="col-right right">
+          <div className="row top"></div>
+          <div className="row bottom"></div>
+        </div>
         
+      </div>
       </div>
     )
 }
