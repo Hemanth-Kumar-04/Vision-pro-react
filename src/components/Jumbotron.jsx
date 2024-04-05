852a3c31b9a71284c220603734cf2d6dcc7a80f5
Hemanth-Kumar-04 <hemanthkumar.k.r04@gmail.com>
2024-04-05 11:13:12 +0530

    jumbotron sectionsd

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function SoundSection(){
+    return(
+        <div className="sound-section wrapper">
+            <div className="body">
+                <div className="sound-section-content content">
+                    <h2 className="title">Lorem ipsum dolor</h2>
+                    <p className="text">Feel The Comfort</p>
+                    <span className="description">
+                       Lorem ipsum dolor sit amet, consectetur adipiscing 
+                    </span>
+                    <ul className="links">
+                        <li>
+                            <button className="button">Buy</button>
+                        </li>
+                        <li>
+                            <a className="link"  >Learn more</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SoundSection;
